e6eed1f98b55cb0576ee020cc49400e6fde003f8
Yevhen Lytvynchuk <95911893+evglitik@users.noreply.github.com>
2022-06-11 17:03:18 +0300

    video-palyer fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,22 @@ const player = new Player(iframe);
 
 const PLAY_VIDEO_TIME = 'videoplayer-current-time';
 
-player.setCurrentTime(localStorage.getItem(PLAY_VIDEO_TIME));
+player
+  .setCurrentTime(localStorage.getItem(PLAY_VIDEO_TIME))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });;
 
 player.on('timeupdate', throttle(handleTimePlay, 1000));
 
